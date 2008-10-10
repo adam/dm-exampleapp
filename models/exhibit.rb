8f4f8957648f43b798a46c3f4bbbb599e6734c75
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-10-10 12:33:18 -0700

    Added missing Exhibit model

---
+++ models/exhibit.rb
@@ -0,0 +1,24 @@
+class Exhibit
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+
+  belongs_to :zoo
+
+  def self.setup
+    san_diego = Zoo.first(:name => 'San Diego Zoo')
+    Exhibit.create(:name => 'Pandas', :zoo => san_diego)
+    Exhibit.create(:name => 'Cheetas', :zoo => san_diego)
+    Exhibit.create(:name => 'Lions', :zoo => san_diego)
+
+    woodland_park = Zoo.first(:name => 'Woodland Park Zoo')
+    Exhibit.create(:name => 'Wolves', :zoo => woodland_park)
+    Exhibit.create(:name => 'Sea Otter', :zoo => woodland_park)
+
+    oregon_zoo = Zoo.first(:name => 'Oregon Zoo')
+    Exhibit.create(:name => 'Elephants', :zoo => oregon_zoo)
+    Exhibit.create(:name => 'Cheetas', :zoo => oregon_zoo)
+    Exhibit.create(:name => 'Zebras', :zoo => oregon_zoo)
+    Exhibit.create(:name => 'Bears', :zoo => oregon_zoo)
+  end
+end
