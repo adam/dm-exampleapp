ce63816b7e39e0fcbcc04464dab7b3dd7b7c58bb
adam <afrench@wieck.com>
2008-10-10 11:32:27 -0500

    init

---
+++ app.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'dm-core'
+
+DataMapper.setup :default, "sqlite3:///tmp/dm-exampleapp.sqlite3.db"
+
+puts <<-EOF
+
+Welcome to the DM Example application.  This script demonstrates the concepts shown at 
+
+http://datamapper.org
+
+
+EOF
